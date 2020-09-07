52b6aa957703d70989000fdcf092ab417aeeed02
Mirafzal Shavkatov <shavkatovm99@gmail.com>
2020-09-07 16:52:15 +0500

    Update db_connect.php

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,1 +1,34 @@
-<?php/*подключение к базе данных*/$host = "localhost"; // в 90% случаев это менять не надо$password = "wKswoI*1";$username = "bunnypeace_getfo";$databasename = "bunnypeace_getfo";global $db;setlocale(LC_ALL,"ru_RU.UTF8");$db = new mysqli($host, $username, $password, $databasename, 3306);$db->set_charset('utf8mb4');if ($db->connect_errno) {    echo "Не удалось подключиться к MySQL: (" . $db->connect_errno . ") " . $db->connect_error;	exit;}
+<?php
+
+/*подключение к базе данных*/
+
+
+
+$host = "localhost"; // в 90% случаев это менять не надо
+
+$password = "wKswoI*1";
+
+$username = "bunnypeace_getfo";
+
+$databasename = "bunnypeace_getfo";
+
+
+
+global $db;
+
+
+
+setlocale(LC_ALL,"ru_RU.UTF8");
+
+
+
+$db = new mysqli($host, $username, $password, $databasename, 3306);
+$db->set_charset('utf8mb4');
+
+if ($db->connect_errno) {
+
+    echo "Не удалось подключиться к MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
+
+	exit;
+
+}
